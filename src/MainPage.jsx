deaf02464686f84b1730c313ef0df62433d38cc8
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2025-01-08 04:49:26 +0100

    dist

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -3,11 +3,11 @@ import Hero from './components/hero/Hero.jsx'
 import SectionOne from './components/sections/sectionOne/SectionOne.jsx'
 import SectionTwo from './components/sections/sectionTwo/sectionTwo.jsx'
 import SectionThree from './components/sections/sectionThree/sectionThree.jsx'
-import Form from "./components/sections/form/Form.jsx"
 import NavBar from './components/navBar/NavBar.jsx'
+import Form from './components/sections/form/Form.jsx'
 const MainPage = () => {
     return (
-        <>  
+        <>
             <NavBar />
             <Hero />
             <SectionOne />
