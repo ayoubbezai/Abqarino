7cca50115a77b19414d9304932f13895ac6685a2
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2025-01-08 19:29:03 +0100

    forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Teacher from "./Teacher  Page/Teacher.jsx";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Teacher from "./Teacher Page/Teacher.jsx";
 import { LanguageProvider } from './context/LanguageContext';
-import MainPage from '../src/MainPage.jsx';
+import MainPage from './MainPage.jsx';
 import { Footer } from './components/footer/Footer.jsx';
 import ScrollToTop from './components/helpful/ScrollToTop.jsx';
 
 const App = () => {
     return (
         <LanguageProvider>
-            <BrowserRouter>
+            <Router>
                 <ScrollToTop />
 
                 <Routes>
@@ -17,7 +17,7 @@ const App = () => {
                 </Routes>
 
                 <Footer />
-            </BrowserRouter>
+            </Router>
         </LanguageProvider>
     );
 };
